81606b41f95081bd96b9330f2b14b7210fa2c6a2
Shadow073180 <david73180@gmail.com>
2019-12-04 22:22:46 -0500

    added a navbar

--- pet-connections/src/App.js
+++ pet-connections/src/App.js
@@ -6,12 +6,26 @@ import Login from './components/Login'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+
+  
   return (
-    <BrowserRouter>
-    <div>
-      <Route exact path="/login" component={Login}/>
+    <div> 
+      <div align='center'>
+        <h1><b>PET CONNECTIONS</b></h1>
+      </div>
+      <hr />
+      <div align='center'>
+        <button a href='#'>Messages</button><button a href='#'>Profile</button><button a href='#'>Account</button><button a href='#'>Search</button><button a href='#' align='right'>LogOut</button>   
+      </div>
+      <hr />
+      <div>
+        <BrowserRouter>
+        <div>
+          <Route exact path="/login" component={Login}/>
+        </div>
+        </BrowserRouter>
+      </div>
     </div>
-    </BrowserRouter>
   );
 }
 
